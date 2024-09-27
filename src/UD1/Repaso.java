b1e11efaeccd83b3349e2afe99072f47f7ee3445
pabloptri <arzmoevius@gmail.com>
2024-09-27 16:01:36 +0200

    En que estrella estara

---
+++ src/UD1/Repaso.java
@@ -0,0 +1,28 @@
+package UD1;
+
+/**
+ * Dos formas para crear procesos
+ * -ProccesBuilder
+ * -Runtime
+ *
+ * -Procces:
+ * -waitFor()-> esoera y devuelve codigo
+ * -isAlive()-> proceso activo
+ *
+ * -getInputStream()-> Salida
+ * -getOutputStream()->entrada
+ * -getErrorStream()->Salida
+ *
+ * ProccesBuiolder
+ * -redirectInput -> escribir desde el intelli
+ * -redirectOutput ->redirijo la salida de proceso al intelli
+ * -redirectError -> Mostrare el error en el intelli
+ *
+ * InheritIO();
+ *
+ * //Nombrado
+ *
+ * //Excepciones
+ */
+public class Repaso {
+}
